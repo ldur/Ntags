9cf18ca06679fd2cef487666bcab3ab727760973
ldur <lasse@durucz.com>
2025-02-09 15:53:08 +0100

    Initial Commit

---
+++ Ntags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ntags
+//
+//  Created by Lasse Durucz on 09/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ntags/NtagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NtagsApp.swift
+//  Ntags
+//
+//  Created by Lasse Durucz on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct NtagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
